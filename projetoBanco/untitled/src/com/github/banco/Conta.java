4b2d8ad5ecda5594d714348e2478336644bd7281
Fagner Tranquilino <fagnertds@gmail.com>
2024-09-09 14:55:44 -0300

    projeto iniciado

--- projetoBanco/untitled/src/com/github/banco/Conta.java
+++ projetoBanco/untitled/src/com/github/banco/Conta.java
@@ -1,4 +1,63 @@
 package com.github.banco;
 
 public class Conta {
+
+    private Titular titular;
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public Titular getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Titular titular) {
+        this.titular = titular;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void sacar(double valorSaque) {
+        if (valorSaque < 0) {
+            throw new IllegalArgumentException("valor do saque deve ser maior que 0");
+        }
+        if (valorSaque > getSaldo()) {
+            throw new IllegalArgumentException("Não há saldo para o valor de saque");
+        }
+        saldo-= valorSaque;
+    }
+
+    public void depositar(double valorDeposito) {
+        if (valorDeposito < 0) {
+            throw new IllegalArgumentException("valor do saque deve ser maior que 0");
+        }
+        saldo += valorDeposito;
+    }
+
+    public void imprimeirDemosntrativo() {
+        System.out.println();
+        System.out.printf("Agência: %d%n", getAgencia());
+        System.out.printf("Conta: %d%n", getNumero());
+        System.out.printf("Titular: %s%n", getTitular().getNome());
+        System.out.printf("Saldo: %.2f%n", getSaldo());
+    }
+
 }
--- projetoBanco/untitled/src/com/github/banco/Principal.java
+++ projetoBanco/untitled/src/com/github/banco/Principal.java
@@ -3,5 +3,19 @@ package com.github.banco;
 public class Principal {
     public static void main(String[] args) {
 
+        Conta conta1 = new Conta();
+        conta1.setTitular(new Titular("Fagner Tranquis", "44499955520"));
+        conta1.setAgencia(123);
+        conta1.setNumero(999999);
+
+        conta1.imprimeirDemosntrativo();
+
+        conta1.depositar(100);
+        conta1.imprimeirDemosntrativo();
+
+        conta1.sacar(25);
+        conta1.imprimeirDemosntrativo();
+
+
     }
 }
