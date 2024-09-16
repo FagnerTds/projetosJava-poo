613ff2d171fe98a504b4104abf8ab4eaa1940ff1
Fagner Tranquilino <fagnertds@gmail.com>
2024-09-16 13:03:47 -0300

    implemetação dos toString

---
+++ projetoBanco/untitled/src/Principal4.java
@@ -0,0 +1,20 @@
+import com.github.banco.Conta;
+import com.github.banco.ContaEspecial;
+import com.github.banco.ContaInvestimento;
+import com.github.banco.Titular;
+
+public class Principal4 {
+    public static void main(String[] args) {
+
+        Titular titular = new Titular("Fagner Tranquis", "44499955520");
+        Conta conta1 = new Conta(titular,123,9999999);
+
+        ContaInvestimento conta2 = new ContaInvestimento(titular,123,9999999);
+
+        ContaEspecial conta3 = new ContaEspecial(titular,123,9999999,90);
+
+
+
+        System.out.println(conta3);
+    }
+}
